753569f76c5c48dd2d2b33bdf44d92eda29a429b
Samarth Dhawan <83126069+samarth2812@users.noreply.github.com>
2022-10-10 17:38:52 +0530

    Update LoggerTest.php

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -6,6 +6,7 @@
  * @subpackage Tests
  *
  * @link https://github.com/utopia-php/framework
+ *
  * @version 1.0 RC4
  * @license The MIT License (MIT) <http://www.opensource.org/licenses/mit-license.php>
  */
