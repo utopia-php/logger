7e29744d4a6f605bdff52c1ed3764265c0026f52
loks0n <22452787+loks0n@users.noreply.github.com>
2024-12-13 17:45:31 +0000

    fix: logowltest

--- tests/e2e/Adapter/LogOwlTest.php
+++ tests/e2e/Adapter/LogOwlTest.php
@@ -12,6 +12,6 @@ class LogOwlTest extends AdapterBase
         parent::setUp();
         $logOwlKey = \getenv('TEST_LOGOWL_KEY');
         $this->adapter = new LogOwl($logOwlKey ? $logOwlKey : '');
-        $this->invalidAdapter = new LogOwl('abc');
+        $this->invalidAdapter = new LogOwl('abc', 'https://api.invalid.io/logging/');
     }
 }
